f4f80016fa9a8e1ebca77e182e2ad8e4c1d6776a
MoonSangWon <kuim75@naver.com>
2022-12-25 19:39:59 +0900

    chore: 생성자 주입 추가

--- src/main/kotlin/Winter/pets/controller/TestController.kt
+++ src/main/kotlin/Winter/pets/controller/TestController.kt
@@ -1,16 +1,12 @@
 package Winter.pets.controller
 
-import Winter.pets.domain.Country.GunGu
-import Winter.pets.domain.Country.Si
 import Winter.pets.service.PetService
-import Winter.pets.service.PetServiceImpl
+import io.swagger.annotations.ApiOperation
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.repository.query.Param
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.lang.RuntimeException
 
 @RestController
 @RequestMapping("/api")
@@ -23,16 +19,37 @@ class TestController {
     }
 
     }
-    @Operation(summary = "시 찾기")
-    @GetMapping("/si")
-    fun  ResponseBody(): List<Si> {
-        val list = petService.findCountry();
-        return list;
+   /* @Operation(summary = "시 db에 저장")
+    @GetMapping("/add/si")
+    fun AddToSi(): ResponseEntity<String>{
+        try {
+            val list = petService.addToCountry();
+            return ResponseEntity.ok().body("시/광역시가 추가되었습니다.")
+        }catch (e:RuntimeException){
+            return ResponseEntity.badRequest().body("잘못된 조회")
+        }
+
     }
+
+    @Operation(summary="시에 대한 군구 db에 저장")
+    @GetMapping("/add/gungu")
+    fun AddToGungu(name:String): ResponseEntity<String>{
+        try{
+            val list = petService.addToGu(name);
+            return ResponseEntity.ok().body("군/구가 추가되었습니다.")
+        }catch (e: RuntimeException){
+            return ResponseEntity.badRequest().body("잘못된 조회")
+        }
+
+    }*/
     @Operation(summary = "시에 대한 군구 찾기")
-    @GetMapping("/gungu")
-    fun ResponseBody(@RequestParam("GunGu_name") name:String): List<String> {
-        val list = petService.findGu(name);
-        return list;
+    @PostMapping("/find/gungu")
+    fun findToGungu(@RequestParam("Si_name") name:String): ResponseEntity<Any> {
+        try{
+            val list = petService.findToGu(name);
+            return ResponseEntity.ok().body(list);
+        }catch (e: RuntimeException){
+            return ResponseEntity.badRequest().body("잘못된 조회")
+        }
     }
 }
