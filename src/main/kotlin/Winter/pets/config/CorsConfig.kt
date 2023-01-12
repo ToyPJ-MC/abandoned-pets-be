d70e3d876cb4a480629f382718d5cda6a1a4268e
MoonSangWon <kuim75@naver.com>
2023-01-12 23:02:38 +0900

    fix : cors config 수정

--- src/main/kotlin/Winter/pets/config/CorsConfig.kt
+++ src/main/kotlin/Winter/pets/config/CorsConfig.kt
@@ -1,8 +1,12 @@
 package Winter.pets.config
 
 import io.swagger.models.HttpMethod
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.context.request.async.TimeoutCallableProcessingInterceptor
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -12,7 +16,7 @@ class CorsConfig : WebMvcConfigurer {
     override
     fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/api")
-            .allowedOrigins("http://203.241.228.50:18000")
+            .allowedOrigins("http://localhost:8080")
             .allowedOrigins("http://localhost:5173")
             .allowCredentials(true)
             .allowedMethods("*")
@@ -25,5 +29,18 @@ class CorsConfig : WebMvcConfigurer {
     }
     fun timeoutInterceptor():TimeoutCallableProcessingInterceptor{
         return TimeoutCallableProcessingInterceptor()
+    }
+    @Bean
+    fun corsConfigurationSource() : CorsConfigurationSource{
+        val cors = CorsConfiguration()
+        cors.addAllowedOrigin("http://localhost:5173")
+        cors.addAllowedOrigin("http://localhost:8080")
+        cors.addAllowedMethod("*")
+        cors.addAllowedHeader("*")
+        cors.setMaxAge(7200L)
+        val url =UrlBasedCorsConfigurationSource()
+        url.registerCorsConfiguration("/**",cors)
+        return url
+
     }
 }
