d494a52b30fecd09134c5112595a829b84c0645d
MoonSangWon <kuim75@naver.com>
2023-01-30 19:36:32 +0900

    fix : 토큰 값 헤더 저장 및 유조 조회 기능 통합

--- src/main/kotlin/Winter/pets/controller/KakaoController.kt
+++ src/main/kotlin/Winter/pets/controller/KakaoController.kt
@@ -2,6 +2,7 @@ package Winter.pets.controller
 
 import Winter.pets.domain.jwt.KakaoService
 import io.swagger.v3.oas.annotations.Operation
+import org.springframework.http.ResponseCookie
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -18,35 +19,20 @@ class KakaoController(private val kakaoService: KakaoService) {
 
         println("code = $code")
         val token =kakaoService.getToken(code)
-        var accessToken = Cookie("access_token",token.accessToken)
-        //accessToken.isHttpOnly = true
-        accessToken.maxAge = token.accessExpiresIn
-        accessToken.path="/"
-        response.addCookie(accessToken)
-
-        var refreshToken = Cookie("refresh_token",token.refreshToken)
-        //refreshToken.isHttpOnly = true
-        refreshToken.maxAge = token.refreshExpiresIn
-        refreshToken.path = "/"
-        response.addCookie(refreshToken)
-
-        response.setHeader("Set-Cookie",refreshToken.value)
-
-        return ResponseEntity.ok().body("저장")
-    }
-    @Operation(summary = "access_token으로 유저 정보 요청")
-    @PostMapping("/user/info")
-    fun getUserinfo(request: HttpServletRequest):ResponseEntity<Any>{
-        try{
-            val cookies : Array<Cookie> = request.cookies
-            var agree:String?=null
-            for (i in 0 until cookies.size)
-                if(cookies[i].name.equals("accessToken"))
-                    agree = kakaoService.getUserInfo(cookies[i].value).toString()
-            return ResponseEntity.ok().body(agree)
-        }catch (e : RuntimeException){
-            return ResponseEntity.badRequest().body("잘못된 조회")
-        }
+        var accessToken = ResponseCookie.from("access_token", token.accessToken.toString())
+            .httpOnly(true)
+            .sameSite("None")
+            .secure(true)
+            .build()
+        var refreshToken = ResponseCookie.from("refresh_token", token.refreshToken.toString())
+            .httpOnly(true)
+            .sameSite("None")
+            .secure(true)
+            .build()
+        response.addHeader("access_token",accessToken.toString())
+        response.addHeader("refresh_token",refreshToken.toString())
+        var list  = kakaoService.getUserInfo(token.accessToken.toString())
+        return ResponseEntity.ok().body(list)
     }
     @Operation(summary = "약관 정보")
     @PostMapping("/user/test")
