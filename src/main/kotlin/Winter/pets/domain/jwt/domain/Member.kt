17052ae7c003e4d639948ba6b2544487cac09a4a
MoonSangWon <kuim75@naver.com>
2023-04-07 20:25:42 +0900

    fix : token수정, role 추가

--- src/main/kotlin/Winter/pets/domain/jwt/domain/Member.kt
+++ src/main/kotlin/Winter/pets/domain/jwt/domain/Member.kt
@@ -4,10 +4,13 @@ import Winter.pets.domain.entity.Likes
 import Winter.pets.domain.kind.Pet
 import io.swagger.annotations.ApiModelProperty
 import lombok.Data
+import lombok.Generated
+import java.util.UUID
 import javax.persistence.CascadeType
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.FetchType
+import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.persistence.OneToMany
 
@@ -15,16 +18,16 @@ import javax.persistence.OneToMany
 @Data
 class Member {
     @Id
-    @Column(name="member_id")
-    @ApiModelProperty(value = "멤버 고유 ID")
-    var id:String?=null
+    @Column(name="member_id", nullable = false)
+    private val id =UUID.randomUUID().toString()
     @Column(name="memeber_name")
     var name:String?=null
     @Column(name="member_email")
     var email:String?=null
     @Column(name="member_profile")
     var profile:String?=null
-    var accessToken:String?=null
+    var refreshToken:String?=null
+    var role = "ROLE_USER"
     @OneToMany(cascade = arrayOf(CascadeType.ALL), fetch = FetchType.EAGER)
     var list:MutableList<Pet> = ArrayList<Pet>()
 }
